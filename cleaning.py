6eb513ab5517d784c4234a52ec0bd8bac8182992
Vaishnavi Saravanan <75262426+VaishnaviSaravanan2801@users.noreply.github.com>
2020-12-20 09:47:30 -0800

    Update and rename Project 1 DA using Pandas Scikit_learn.py to cleaning.py

---
+++ cleaning.py
@@ -0,0 +1,18 @@
+#Import the model
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#Load the dataset
+df = pd.read_csv('F:\Suven Intern\WeatherHistory.csv')
+df.head()
+# Data Preparation
+df.columns
+df.shape
+df.size
+df.describe()
+df.info()
+df.dtypes
+# Data Cleaning
+df=df.drop(['Summary','Precip Type','Temperature (C)','Wind Speed (km/h)',
+            'Wind Bearing (degrees)','Visibility (km)','Pressure (millibars)','Daily Summary'], axis=1)
+df.head()
